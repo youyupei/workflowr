82c7df32885775ffc2ad29f57746e66ce132c52d
youyupei <youyupei@gmail.com>
2019-05-13 22:01:13 +1000

    update

--- analysis/workflowr.R
+++ analysis/workflowr.R
@@ -2,3 +2,13 @@ library(workflowr)
 wflow_build()
 wflow_publish("*.Rmd")
 wflow_git_push()
+
+
+
+## Build all files
+for(i in list.files())
+{
+  if(grepl(".Rmd", i)){
+    try(wflow_build(i))
+  }
+}
