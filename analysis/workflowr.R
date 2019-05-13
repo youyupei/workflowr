caa1d34c6331fb9cc197f5e1cc3eb17f9726814c
youyupei <youyupei@gmail.com>
2019-05-13 22:07:19 +1000

    Revert "update"
    This reverts commit 82c7df32885775ffc2ad29f57746e66ce132c52d.

--- analysis/workflowr.R
+++ analysis/workflowr.R
@@ -2,13 +2,3 @@ library(workflowr)
 wflow_build()
 wflow_publish("*.Rmd")
 wflow_git_push()
-
-
-
-## Build all files
-for(i in list.files())
-{
-  if(grepl(".Rmd", i)){
-    try(wflow_build(i))
-  }
-}
